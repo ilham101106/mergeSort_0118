6ffd8cec08f87dd941ce8b0dfec0793f03354fb3
ilham101106 <pesatoffical321@gmail.com>
2025-04-17 17:20:39 +0700

    tambah display sesuai element array

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -5,3 +5,26 @@ int arr[20], B[20];
 
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukkan Panjang element array:";
+        cin >> n;
+
+        if (n<= 20) {
+            break;
+        }
+        else {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
+    cout << "\n===================";
+    cout << "\nInputkan Isi Element Array";
+    cout << "\n===================" << endl;
+
+    for (int i = 0; i < n; i++ ) {
+        cout << "Array index ke - " << i << ": ";
+        cin >> arr[i];
+    }
+}
+
